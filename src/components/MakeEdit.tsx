83ea54c2e75078b1958048c869a05bfac610ea9a
Ayoola Olumide Olufemi <ayoolaolumide98@gmail.com>
2025-03-29 16:27:36 +0100

    minor styling

--- src/components/MakeEdit.tsx
+++ src/components/MakeEdit.tsx
@@ -8,7 +8,7 @@ const MakeEdit = ({prev, onCLick} : {prev: string, onCLick: (content: string) =>
   return (
     <section className="flex justify-between items-center mt-5 relative">
         {isEmpty && <p className="text-(--Soft-Red) text-sm  absolute top-[-20px] left-[16px]">Comment cannot be empty</p>}
-        <textarea value={content}  onChange={(e) => setContent(e.target.value)} className="outline-1 outline-(--Grayish-Blue) w-full mx-5 p-2" placeholder="Update Comment" name="mew-comment" id="new-comment"></textarea>
+        <textarea value={content}  onChange={(e) => setContent(e.target.value)} className="outline-1 outline-(--Grayish-Blue) w-full mx-5 p-2 rounded-lg" placeholder="Update Comment" name="mew-comment" id="new-comment"></textarea>
         <button onClick={() => {
             if(content.length == 0){
                 setIsEmpty(true)
