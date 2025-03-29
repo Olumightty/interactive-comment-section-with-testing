7b0fb472fb3a5195dd0a61a37a1ab032e03bf153
Ayoola Olumide Olufemi <ayoolaolumide98@gmail.com>
2025-03-29 16:24:14 +0100

    fix: vote button icons

--- src/components/VoteButton.tsx
+++ src/components/VoteButton.tsx
@@ -3,9 +3,9 @@
 const VoteButton = ({score, onVote}: {score: number, onVote: (type: 'up' | 'down') => void}) => {
   return (
     <div className='p-3 self-start flex flex-row md:flex-col gap-5 bg-(--Very-light-gray) justify-center items-center rounded-t-lg rounded-b-lg'>
-        <img className="cursor-pointer" onClick={() => onVote('up')} src="public/images/icon-plus.svg" alt="plus"/>
+        <img className="cursor-pointer" onClick={() => onVote('up')} src="/images/icon-plus.svg" alt="plus"/>
         <p className="text-(--Moderate-blue)" role="voting-score">{score}</p>
-        <img src="public/images/icon-minus.svg" alt="minus" className="cursor-pointer" onClick={() => onVote('down')}/>
+        <img src="/images/icon-minus.svg" alt="minus" className="cursor-pointer" onClick={() => onVote('down')}/>
 
     </div>
   )
